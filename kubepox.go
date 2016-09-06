25bab639ea9c235a5ef8b60705cb92ac549c2d62
Bernard Van De Walle <bernard@bvandewa.com>
2016-09-06 11:35:31 -0700

    The kubepox package doesn't depend on a REST client anymore. The functions have been refactored so that it expects ListofPolicies and ListOfPods as input instead than a restClient

--- kubepox.go
+++ kubepox.go
@@ -45,17 +45,12 @@ func PrintPods(c *client.Client) error {
 }
 
 // ListPoliciesPerPod returns all the NetworkPolicies that are associated with a pod.
-func ListPoliciesPerPod(c *client.Client, pod *api.Pod) (*extensions.NetworkPolicyList, error) {
+func ListPoliciesPerPod(pod *api.Pod, allPolicies *extensions.NetworkPolicyList) (*extensions.NetworkPolicyList, error) {
 
 	matchedPolicies := extensions.NetworkPolicyList{
 		Items: []extensions.NetworkPolicy{},
 	}
 	podLabels := labels.Set(pod.GetLabels())
-
-	allPolicies, err := c.Extensions().NetworkPolicies("").List(api.ListOptions{})
-	if err != nil {
-		return nil, err
-	}
 
 	// Iterate over all policies and find the one that apply to the pod.
 	for _, policy := range allPolicies.Items {
@@ -71,18 +66,24 @@ func ListPoliciesPerPod(c *client.Client, pod *api.Pod) (*extensions.NetworkPoli
 	return &matchedPolicies, nil
 }
 
-// ListPodsPerPolicy returns all the Pods that are affected by a policy.
-func ListPodsPerPolicy(c *client.Client, np *extensions.NetworkPolicy) (*api.PodList, error) {
+// ListPodsPerPolicy returns all the Pods that are affected by a policy out of the list.
+func ListPodsPerPolicy(np *extensions.NetworkPolicy, allPods *api.PodList) (*api.PodList, error) {
 
 	selector, err := apiu.LabelSelectorAsSelector(&np.Spec.PodSelector)
 	if err != nil {
 		return nil, err
 	}
 
-	// Match pods based on the Label Selector that came with the policy
-	matchedPods, err := c.Pods("").List(api.ListOptions{LabelSelector: selector})
-	if err != nil {
-		return nil, err
+	matchedPods := api.PodList{
+		Items: []api.Pod{},
 	}
-	return matchedPods, nil
+
+	// Match pods based on the Label Selector that came with the policy
+	for _, pod := range allPods.Items {
+		if selector.Matches(labels.Set(pod.GetLabels())) {
+			matchedPods.Items = append(matchedPods.Items, pod)
+		}
+	}
+
+	return &matchedPods, nil
 }
