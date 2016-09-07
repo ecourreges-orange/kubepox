51275caf3386d4f77d1fb633579b0b0136fb332e
Bernard Van De Walle <bernard@bvandewa.com>
2016-09-06 22:00:02 -0700

    cleaning CLI

--- cmd/kubepox/main.go
+++ cmd/kubepox/main.go
@@ -25,8 +25,10 @@ func main() {
 	usage := `
 
 	Usage:
-	kubepox [--config <config>][--namespace <namespace>] get (np|pod)
-	kubepox [--config <config>][--namespace <namespace>] affect (np|pod) (<name>)
+  kubepox [--config <config>][--namespace <namespace>] get-all (policies|pods)
+  kubepox [--config <config>][--namespace <namespace>] get-pods <policy>
+  kubepox [--config <config>][--namespace <namespace>] get-policies <pod>
+  kubepox [--config <config>][--namespace <namespace>] get-rules <pod>
 
   Options:
 	--namespace=NAMESPACE Namespace to run the query in
@@ -34,8 +36,8 @@ func main() {
 	`
 
 	arguments, _ := docopt.Parse(usage, nil, true, "Naval Fate 2.0", false)
-	fmt.Println(arguments)
 
+	// Get location of the Kubeconfig file. By default in your home.
 	var kubeconfig string
 	if arguments["--config"] == nil {
 		kubeconfig = os.Getenv("HOME") + "/.kube/config"
@@ -43,13 +45,13 @@ func main() {
 		kubeconfig = arguments["--config"].(string)
 	}
 
+	// Get namespace, by default it will be "default"
 	var namespace string
 	if arguments["--namespace"] == nil {
-		namespace = ""
+		namespace = "default"
 	} else {
 		namespace = arguments["--namespace"].(string)
 	}
-	fmt.Println(namespace)
 
 	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
 	if err != nil {
@@ -63,32 +65,54 @@ func main() {
 		os.Exit(1)
 	}
 
-	if arguments["get"].(bool) && arguments["np"].(bool) {
-		kubepox.PrintPolicies(myClient)
-	} else if arguments["get"].(bool) && arguments["pod"].(bool) {
-		kubepox.PrintPods(myClient)
-	} else if arguments["affect"].(bool) && arguments["np"].(bool) {
-		// Get the Policy in argument
-		np, err := myClient.Extensions().NetworkPolicies(namespace).Get(arguments["<name>"].(string))
+	// Display all policies. Similar to kubectl describe policies in json
+	if arguments["get-all"].(bool) && arguments["policies"].(bool) {
+
+		policies, err := myClient.Extensions().NetworkPolicies(namespace).List(api.ListOptions{})
 		if err != nil {
 			fmt.Printf("Couldn't get Network Policy: %v\n", err)
+			os.Exit(1)
+		}
+		renderPolicies(policies)
+
+		// Display all pods. Similar to kubectl describe pods in json
+	} else if arguments["get-all"].(bool) && arguments["pods"].(bool) {
+
+		pods, err := myClient.Pods(namespace).List(api.ListOptions{})
+		if err != nil {
+			fmt.Printf("Couldn't get all the pods %v\n", err)
+			os.Exit(1)
+		}
+		renderPods(pods)
+
+		// Get all the pods that get affected by the policy
+	} else if arguments["get-pods"].(bool) {
+		// Get the Policy in argument
+		np, err := myClient.Extensions().NetworkPolicies(namespace).Get(arguments["<policy>"].(string))
+		if err != nil {
+			fmt.Printf("Couldn't get Network Policy: %v\n", err)
+			os.Exit(1)
 		}
 		allPods, err := myClient.Pods(namespace).List(api.ListOptions{})
 		if err != nil {
 			fmt.Printf("Couldn't get all the pods %v\n", err)
+			os.Exit(1)
 		}
 		matchedPods, err := kubepox.ListPodsPerPolicy(np, allPods)
 		if err != nil {
 			fmt.Printf("Error getting matching pods: %v\n", err)
+			os.Exit(1)
 		}
-		fmt.Println("Resulting Pods:")
-
+		fmt.Printf("Matched pods for policy %s :\n", np.Name)
 		renderPods(matchedPods)
 
-	} else if arguments["affect"].(bool) && arguments["pod"].(bool) {
-		pod, err := myClient.Pods(namespace).Get(arguments["<name>"].(string))
+		// Get all the policies that get applied to a Pod.
+	} else if arguments["get-policies"].(bool) {
+
+		pod, err := myClient.Pods(namespace).Get(arguments["<pod>"].(string))
 		if err != nil {
 			fmt.Printf("Couldn't get target pod %v\n", err)
+			os.Exit(1)
 		}
 
 		allPolicies, err := myClient.Extensions().NetworkPolicies(namespace).List(api.ListOptions{})
@@ -99,26 +123,57 @@ func main() {
 		matchedPolicies, err := kubepox.ListPoliciesPerPod(pod, allPolicies)
 		if err != nil {
 			fmt.Printf("Error getting matching policies: %v\n", err)
+			os.Exit(1)
 		}
-		fmt.Println("Resulting Policies:")
+		fmt.Printf("Applied policies for pod %s :\n", pod.Name)
 		renderPolicies(matchedPolicies)
 
-	}
+		// Get all the IngressRules that get applied to a Pod.
+	} else if arguments["get-rules"].(bool) {
 
+		pod, err := myClient.Pods(namespace).Get(arguments["<pod>"].(string))
+		if err != nil {
+			fmt.Printf("Couldn't get target pod %v\n", err)
+			os.Exit(1)
+		}
+
+		allPolicies, err := myClient.Extensions().NetworkPolicies(namespace).List(api.ListOptions{})
+		if err != nil {
+			fmt.Printf("Couldn't get all Network Policies: %v\n", err)
+			os.Exit(1)
+		}
+
+		matchedRules, err := kubepox.ListIngressRulesPerPod(pod, allPolicies)
+		if err != nil {
+			fmt.Printf("Couldn't get all the rules: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Printf("Applied rules for pod %s :\n", pod.Name)
+		renderIngressRules(matchedRules)
+
+	}
 }
 
 func renderPolicies(policies *extensions.NetworkPolicyList) {
-	for _, policy := range policies.Items {
-		fmt.Printf("\n\n\n\n")
+	for count, policy := range policies.Items {
+		fmt.Printf("POLICY %d\n", count+1)
 		pp, _ := json.MarshalIndent(&policy, "", "   ")
 		fmt.Println(string(pp))
 	}
 }
 
 func renderPods(pods *api.PodList) {
-	for _, pod := range pods.Items {
-		fmt.Printf("\n\n\n\n")
+	for count, pod := range pods.Items {
+		fmt.Printf("POD %d\n", count+1)
 		pp, _ := json.MarshalIndent(&pod, "", "   ")
+		fmt.Println(string(pp))
+	}
+}
+
+func renderIngressRules(ingressRules *[]extensions.NetworkPolicyIngressRule) {
+	for count, rule := range *ingressRules {
+		fmt.Printf("RULE %d\n", count)
+		pp, _ := json.MarshalIndent(&rule, "", "   ")
 		fmt.Println(string(pp))
 	}
 }
--- kubepox.go
+++ kubepox.go
@@ -1,48 +1,12 @@
 package kubepox
 
 import (
-	"encoding/json"
-	"fmt"
-
 	"k8s.io/kubernetes/pkg/api"
 	"k8s.io/kubernetes/pkg/apis/extensions"
 	"k8s.io/kubernetes/pkg/labels"
 
 	apiu "k8s.io/kubernetes/pkg/api/unversioned"
-	client "k8s.io/kubernetes/pkg/client/unversioned"
 )
-
-// PrintPolicies pretty print all the policies
-func PrintPolicies(c *client.Client) error {
-	policies, err := c.Extensions().NetworkPolicies("").List(api.ListOptions{})
-	if err != nil {
-		return err
-	}
-
-	for _, policy := range policies.Items {
-		fmt.Println("Existing policies:")
-		pp, _ := json.MarshalIndent(&policy, "", "   ")
-		fmt.Println(string(pp))
-	}
-
-	return nil
-}
-
-// PrintPods pretty print all the pods
-func PrintPods(c *client.Client) error {
-	pods, err := c.Pods("").List(api.ListOptions{})
-	if err != nil {
-		return err
-	}
-
-	for _, pod := range pods.Items {
-		fmt.Println("Existing pods:")
-		pp, _ := json.MarshalIndent(&pod, "", "   ")
-		fmt.Println(string(pp))
-	}
-
-	return nil
-}
 
 // ListPoliciesPerPod returns all the NetworkPolicies that are associated with a pod.
 func ListPoliciesPerPod(pod *api.Pod, allPolicies *extensions.NetworkPolicyList) (*extensions.NetworkPolicyList, error) {
